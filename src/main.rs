2fa831c462e36744a42c0f932173e005bcd4c2cf
noreplydev <contact.cristiansanchez@gmail.com>
2023-10-07 19:01:15 +0200

    [FT](Core) Understanding the basic parts of Actix

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,18 @@
 use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
 
-#[get("/")]
-async fn hello() -> impl Responder {
-    HttpResponse::Ok().body("Hello world!")
-}
-
-#[post("/echo")]
-async fn echo(req_body: String) -> impl Responder {
-    HttpResponse::Ok().body(req_body)
-}
-
-async fn manual_hello() -> impl Responder {
-    HttpResponse::Ok().body("Hey there!")
-}
-
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    println!("Web server running at localhost:8080");
+
+    // a thread pool is created here
     HttpServer::new(|| {
+        println!("server threar");
         App::new()
-            .service(hello)
-            .service(echo)
-            .route("/hey", web::get().to(manual_hello))
+            .route("/", web::get().to(|| HttpResponse::Ok()))
+            .route("/hi", web::get().to(|| async { "Hello world!" }))
     })
     .bind(("127.0.0.1", 8080))?
+    .workers(1)
     .run()
     .await
 }
