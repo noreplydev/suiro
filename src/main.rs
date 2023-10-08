20d738c365098a45023192570f0047a79db8bd37
noreplydev <contact.cristiansanchez@gmail.com>
2023-10-08 22:45:08 +0200

    [Understanding actix]

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,14 @@
 use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
 
 #[actix_web::main]
-async fn main() -> std::io::Result<()> {
-    println!("Web server running at localhost:8080");
+async fn main() {
+    println!("main thread");
+    http_server().await;
+    println!("http_socket:");
+}
+
+async fn http_server() -> std::io::Result<()> {
+    println!("Web server running at localhost:3000");
 
     // a thread pool is created here
     HttpServer::new(|| {
@@ -11,7 +17,7 @@ async fn main() -> std::io::Result<()> {
             .route("/", web::get().to(|| HttpResponse::Ok()))
             .route("/hi", web::get().to(|| async { "Hello world!" }))
     })
-    .bind(("127.0.0.1", 8080))?
+    .bind(("127.0.0.1", 3000))?
     .workers(1)
     .run()
     .await
