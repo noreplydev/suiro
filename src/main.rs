2b9c29a281c35defc47fe270d2a014b530953472
noreplydev <contact.cristiansanchez@gmail.com>
2023-10-16 00:57:34 +0200

    [FT](Core) Now the server can run both servers

--- src/main.rs
+++ src/main.rs
@@ -17,37 +17,24 @@ impl Port {
     }
 }
 
-fn main() {
+#[actix_web::main]
+async fn main() {
     println!("[SUIRO] Starting service");
 
     let http_port = Port::new(8080);
     let tcp_port = Port::new(3040);
-
-    // Start the HTTP server
-    // a thread pool is created here
-    /*     let http = HttpServer::new(|| {
-        println!("[HTTP] SERVER STARTED ON PORT: 3000");
-        App::new()
-            .route("/", web::get().to(|| HttpResponse::Ok()))
-            .route("/hi", web::get().to(|| async { "Hello world!" }))
-    })
-    .bind(("127.0.0.1", 3050))
-    .unwrap()
-    .workers(1) // only one thread for the http server
-    .run()
-    .await; */
 
     // Start the TCP server in a new thread
     let tcp = thread::spawn(move || {
         let tcp_server = tcp_server(tcp_port);
     });
 
-    let http = thread::spawn(move || {
-        let http_server = tcp_server(http_port);
-    });
+    // Start the HTTP server in a new thread
+    http_server(http_port)
+        .await
+        .expect("Error starting HTTP server");
 
     tcp.join().unwrap();
-    http.join().unwrap();
 
     println!("[SUIRO] Stopping service");
 }
@@ -70,4 +57,28 @@ fn tcp_server(port: Port) -> Result<std::net::TcpListener, std::io::Error> {
 
 fn handle_connection(stream: TcpStream) {
     println!("[TCP] New connection: {}", stream.peer_addr().unwrap());
+}
+
+async fn http_server(port: Port) -> Result<(), std::io::Error> {
+    // a thread pool is created here
+    HttpServer::new(|| {
+        println!("[HTTP] SERVER STARTED ON PORT: 3000");
+        App::new()
+            .route("/", web::get().to(|| HttpResponse::Ok()))
+            .route(
+                "/hi",
+                web::get().to(|| async {
+                    println!("[HTTP] GET /hi");
+                    HttpResponse::Ok()
+                }),
+            )
+    })
+    .bind(("127.0.0.1", port.num))
+    .unwrap()
+    .workers(1) // only one thread for the http server
+    .run()
+    .await
+    .expect("Error starting HTTP server");
+
+    Ok(())
 }
