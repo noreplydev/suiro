1ff7a791f45a0ca6ed221084a7cc096d93d51ecb
noreplydev <contact.cristiansanchez@gmail.com>
2023-10-28 23:59:02 +0200

    [FT](Core) Littler comment fix

--- src/main.rs
+++ src/main.rs
@@ -23,7 +23,7 @@ fn main() {
         println!("[SUIRO] Stopping service");
         std::process::exit(0);
     })
-    .expect("Error setting Ctrl-C handler");
+    .expect("[SUIRO](ERROR) setting Ctrl-C handler");
 
     let http_port = Port::new(8080);
     let tcp_port = Port::new(3040);
