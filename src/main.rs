01877843bf015ff10c72e57c98326202b4f4da15
noreplydev <contact.cristiansanchez@gmail.com>
2023-11-05 13:48:57 +0100

    [FT] Added cross-thread store for the sessions

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use hyper::service::{make_service_fn, service_fn};
 use hyper::{Body, Response, Server};
-use std::thread;
+use std::sync::{Arc, Mutex};
+use std::{thread, vec};
 use tokio::io::{AsyncBufReadExt, AsyncReadExt, AsyncWriteExt, BufReader};
 use tokio::net::{TcpListener, TcpStream};
 use unique_id::string::StringGenerator;
@@ -19,6 +20,7 @@ impl Port {
     }
 }
 
+#[derive(Debug)]
 struct Session {
     session_id: String,
     session_endpoint: String,
@@ -46,6 +48,10 @@ fn main() {
     let http_port = Port::new(8080);
     let tcp_port = Port::new(3040);
 
+    let mutex: Mutex<Vec<Session>> = Mutex::new(vec![]);
+    let sessions = Arc::new(mutex);
+
+    let sessions_ref_tcp = Arc::clone(&sessions);
     // Spawn threads for each server
     let tcp = thread::spawn(move || {
         let runtime = tokio::runtime::Builder::new_current_thread()
@@ -53,37 +59,44 @@ fn main() {
             .build()
             .unwrap();
 
-        runtime.block_on(async { tcp_server(tcp_port).await });
+        let sessions_ref = Arc::clone(&sessions_ref_tcp);
+        runtime.block_on(async { tcp_server(tcp_port, sessions_ref).await });
     });
 
+    let sessions_ref_http = Arc::clone(&sessions);
     let http = thread::spawn(move || {
         let runtime = tokio::runtime::Builder::new_current_thread()
             .enable_all()
             .build()
             .unwrap();
 
-        runtime.block_on(async { http_server(http_port).await });
+        let sessions_ref = Arc::clone(&sessions_ref_http);
+        runtime.block_on(async { http_server(http_port, sessions_ref).await });
     });
 
     // Wait for the threads to finish
     let _ = tcp.join();
     let _ = http.join();
+
+    println!("{:?}", sessions);
 }
 
-async fn tcp_server(port: Port) {
+async fn tcp_server(port: Port, sessions_ref: Arc<Mutex<Vec<Session>>>) {
     let listener = TcpListener::bind(("127.0.0.1", port.num)).await.unwrap();
     println!("[TCP] Waiting connections on {}", port.num);
 
     loop {
         let (socket, _) = listener.accept().await.unwrap();
+        let sessions_ref_clone = sessions_ref.clone(); // avoid sessions_ref being moved
+
         tokio::spawn(async move {
             // spawn a task for each inbound socket
-            tcp_connection_handler(socket).await;
+            tcp_connection_handler(socket, sessions_ref_clone).await;
         });
     }
 }
 
-async fn tcp_connection_handler(mut stream: TcpStream) {
+async fn tcp_connection_handler(mut stream: TcpStream, sessions_ref: Arc<Mutex<Vec<Session>>>) {
     let gen = StringGenerator::default();
     let session_id = gen.next_id();
     let session_endpoint = gen.next_id();
@@ -95,17 +108,21 @@ async fn tcp_connection_handler(mut stream: TcpStream) {
         .unwrap();
 
     let session = Session::new(session_id, session_endpoint, stream);
+    sessions_ref.lock().unwrap().push(session);
 }
 
-async fn http_server(port: Port) {
+async fn http_server(port: Port, sessions_ref: Arc<Mutex<Vec<Session>>>) {
     // The address we'll bind to.
-    let addr = ([127, 0, 0, 1], 3000).into();
+    let addr = ([127, 0, 0, 1], port.num).into();
 
     // This is our service handler. It receives a Request, processes it, and returns a Response.
     let make_service = make_service_fn(|_conn| {
+        let sessions_ref = Arc::clone(&sessions_ref);
         async {
-            // service_fn converts our function into a `Service`.
-            Ok::<_, hyper::Error>(service_fn(http_connection_handler))
+            Ok::<_, hyper::Error>(service_fn(move |req| {
+                let sessions_clone = sessions_ref.clone();
+                http_connection_handler(req, sessions_clone)
+            }))
         }
     });
 
@@ -119,33 +136,9 @@ async fn http_server(port: Port) {
 
 async fn http_connection_handler(
     _req: hyper::Request<Body>,
+    sessions_ref: Arc<Mutex<Vec<Session>>>,
 ) -> Result<Response<Body>, hyper::Error> {
     println!("[HTTP] New connection {:?}", _req.uri());
+    println!("wiiii --------- {:?}", sessions_ref.lock().unwrap());
     Ok(Response::new(Body::from("Hello, World!")))
 }
-
-////////////////////
-/*
-async fn http_server(port: Port) {
-    let listener = TcpListener::bind(("127.0.0.1", port.num)).await.unwrap();
-    println!("[HTTP] Waiting connections on {}", port.num);
-
-    loop {
-        let (socket, _) = listener.accept().await.unwrap();
-        tokio::spawn(async {
-            // spawn a task for each inbound socket
-            http_connection_handler(socket).await;
-        });
-    }
-}
-
-async fn http_connection_handler(stream: TcpStream) {
-    println!("[HTTP] New connection {}", stream.peer_addr().unwrap());
-    let mut buf_reader = BufReader::new(stream);
-
-    let mut headers: Vec<String> = Vec::new();
-    let mut body: Vec<String> = Vec::new();
-
-    println!("[HTTP] Request headers: \n\n\r{}\n", headers.join("\n"));
-} */
-////////////////////
